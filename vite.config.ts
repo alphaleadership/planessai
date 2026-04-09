b7d0c0fa9264433a418cb67d3c5890439e299a44
alphaleadership <47387699+alphaleadership@users.noreply.github.com>
2026-04-09 08:12:47 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,11 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss()],base: '/NOM_DEPOT/', // Exemple : '/mon-projet-vite/'
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
